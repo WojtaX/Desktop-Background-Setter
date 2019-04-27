2161d1705a32eabd824871ac21ce990b18b5d33d
Sa$ha <wojtek.l@usk.waw.pl>
2019-04-27 13:27:20 +0200

    dodany stringbuilder przy kontroli błędów

--- WallpaperSetter/Logger.cs
+++ WallpaperSetter/Logger.cs
@@ -10,10 +10,11 @@ namespace WallpaperSetter
 	class Logger
 	{
  public static void LogCreator(string message){
-			string Year = DateTime.Today.Year.ToString();
-			string Month = DateTime.Today.Month.ToString();
-			string Day = DateTime.Today.Day.ToString();
-			string date = Year + Month + Day;
+			StringBuilder date = new StringBuilder();
+			date.Append(DateTime.Today.Year.ToString());
+			date.Append(DateTime.Today.Month.ToString());
+			date.Append(DateTime.Today.Day.ToString());
+		
 			System.IO.Directory.CreateDirectory("LOG");
 			using (StreamWriter outputFile = new StreamWriter(Path.Combine(DirectoryGetter.GetCurrentDirectory()+@"\LOG", date+"LOG.txt")))
 			{
